77e0b5b1586e9a6ecb5a95dcc03ba12cc80c8e21
Agustin <agustincigaran@icloud.com>
2025-03-20 15:14:32 -0300

    Hardcodeo trabajos

---
+++ PaginaWeb/Js/logica_trabajo.js
@@ -0,0 +1,162 @@
+function Trabajo (nombreTrabajo, tipoTrabajo, contado, mercadoPago) {
+    this.nombre= nombreTrabajo;
+    this.tipo= tipoTrabajo;
+    this.precioContado= contado;
+    this.precioMercadoPago= mercadoPago;
+}
+
+// ¡¡HARDCODEO!! TODOS LOS TRABAJOS QUE REALZIA EL LABORATORIO
+let acrilico1 = new Trabajo('Protesis Completa', 'Acrilico', 6000, 6600);
+let acrilico2 = new Trabajo('Hasta 2 piezas', 'Acrilico', 3500, 3850);
+let acrilico3 = new Trabajo('De 3 a 5 piezas', 'Acrilico', 4500, 4950);
+let acrilico4 = new Trabajo('De 6 a 10 piezas', 'Acrilico', 5300, 5830);
+let acrilico5 = new Trabajo('Más de 10 piezas', 'Acrilico', 6000, 6600);
+let acrilico6 = new Trabajo('Provisoria hasta 2 piezas', 'Acrilico', 2000, 2200);
+
+let cromo1 = new Trabajo('Hasta 2 piezas', 'Cromo', 10000, 11000);
+let cromo2 = new Trabajo('De 3 a 5 piezas', 'Cromo', 11000, 12100);
+let cromo3 = new Trabajo('Más de 5 piezas', 'Cromo', 12000, 13200);
+
+let reparaciones1 = new Trabajo('Sencillas de fractura', 'Reparaciones', 800);
+let reparaciones2 = new Trabajo('Con molde', 'Reparaciones', 1000);
+let reparaciones3 = new Trabajo('Agregado de diente', 'Reparaciones', 1000);
+let reparaciones4 = new Trabajo('Agregado de gancho', 'Reparaciones', 1000);
+let reparaciones5 = new Trabajo('Placa de contención', 'Reparaciones', 2000, 2200);
+let reparaciones6 = new Trabajo('Placas de blancamiento (inf y sup)', 'Reparaciones', 2000, 2200);
+let reparaciones7 = new Trabajo('Tratam. Blanc. (placas + jeringas)', 'Reparaciones', 4000, 4400);
+
+// =================================
+// ====================== > ACRILICO
+// =================================
+
+// CAMBIAR NOMBRE
+for (i = 1; i < 7; i++) {
+    let temporalNombreA = document.getElementById(`nombreCeldaAcrilico`+i);
+    if (i == 1) {
+        temporalNombreA.innerHTML = acrilico1.nombre;
+    } if (i == 2) {
+        temporalNombreA.innerHTML = acrilico2.nombre;
+    } if (i == 3) {
+        temporalNombreA.innerHTML = acrilico3.nombre;
+    } if (i == 4) {
+        temporalNombreA.innerHTML = acrilico4.nombre;
+    } if (i == 5) {
+        temporalNombreA.innerHTML = acrilico5.nombre;
+    } if (i == 6) {
+        temporalNombreA.innerHTML = acrilico6.nombre;
+    }
+}
+
+// CAMBIAR PRECIO
+for (i = 1; i < 13; i++) {
+    let temporalPrecioA = document.getElementById('HolderAcrilico'+i);
+    if (i == 1) {
+        temporalPrecioA.placeholder = acrilico1.precioContado;
+    } if (i == 2) {
+        temporalPrecioA.placeholder = acrilico1.precioMercadoPago;
+    } if (i == 3) {
+        temporalPrecioA.placeholder = acrilico2.precioContado;
+    } if (i == 4) {
+        temporalPrecioA.placeholder = acrilico2.precioMercadoPago;
+    } if (i == 5) {
+        temporalPrecioA.placeholder = acrilico3.precioContado;
+    } if (i == 6) {
+        temporalPrecioA.placeholder = acrilico3.precioMercadoPago;
+    } if (i == 7) {
+        temporalPrecioA.placeholder = acrilico4.precioContado;
+    } if (i == 8) {
+        temporalPrecioA.placeholder = acrilico4.precioMercadoPago;
+    } if (i == 9) {
+        temporalPrecioA.placeholder = acrilico5.precioContado;
+    } if (i == 10) {
+        temporalPrecioA.placeholder = acrilico5.precioMercadoPago;
+    } if (i == 11) {
+        temporalPrecioA.placeholder = acrilico6.precioContado;
+    } if (i == 12) {
+        temporalPrecioA.placeholder = acrilico6.precioMercadoPago;
+    }
+}
+
+// ==============================
+// ====================== > CROMO
+// ==============================
+
+// CAMBIAR NOMBRE
+for (i = 1; i < 4; i++) {
+    let temporalNombreC = document.getElementById(`nombreCeldaCromo`+i);
+    if (i == 1) {
+        temporalNombreC.innerHTML = cromo1.nombre;
+    } if (i == 2) {
+        temporalNombreC.innerHTML = cromo2.nombre;
+    } if (i == 3) {
+        temporalNombreC.innerHTML = cromo3.nombre;
+    }
+}
+
+// CAMBIAR PRECIO
+for (i = 1; i < 7; i++) {
+    let temporalPrecioC = document.getElementById('HolderCromo'+i);
+    if (i == 1) {
+        temporalPrecioC.placeholder = cromo1.precioContado;
+    } if (i == 2) {
+        temporalPrecioC.placeholder = cromo1.precioMercadoPago;
+    } if (i == 3) {
+        temporalPrecioC.placeholder = cromo2.precioContado;
+    } if (i == 4) {
+        temporalPrecioC.placeholder = cromo2.precioMercadoPago;
+    } if (i == 5) {
+        temporalPrecioC.placeholder = cromo3.precioContado;
+    } if (i == 6) {
+        temporalPrecioC.placeholder = cromo3.precioMercadoPago;
+    }
+}
+
+// =====================================
+// ====================== > REPARACIONES
+// =====================================
+
+// CAMBIAR NOMBRE
+for (i = 1; i < 8; i++) {
+    let temporalNombreR = document.getElementById(`nombreCeldaReparaciones`+i);
+    if (i == 1) {
+        temporalNombreR.innerHTML = reparaciones1.nombre;
+    } if (i == 2) {
+        temporalNombreR.innerHTML = reparaciones2.nombre;
+    } if (i == 3) {
+        temporalNombreR.innerHTML = reparaciones3.nombre;
+    } if (i == 4) {
+        temporalNombreR.innerHTML = reparaciones4.nombre;
+    } if (i == 5) {
+        temporalNombreR.innerHTML = reparaciones5.nombre;
+    } if (i == 6) {
+        temporalNombreR.innerHTML = reparaciones6.nombre;
+    } if (i == 7) {
+        temporalNombreR.innerHTML = reparaciones7.nombre;
+    }
+}
+
+// CAMBIAR PRECIO
+for (i = 1; i < 11; i++) {
+    let temporalPrecioR = document.getElementById('HolderReparaciones'+i);
+    if (i == 1) {
+        temporalPrecioR.placeholder = reparaciones1.precioContado;
+    } if (i == 2) {
+        temporalPrecioR.placeholder = reparaciones2.precioContado;
+    } if (i == 3) {
+        temporalPrecioR.placeholder = reparaciones3.precioContado;
+    } if (i == 4) {
+        temporalPrecioR.placeholder = reparaciones4.precioContado;
+    } if (i == 5) {
+        temporalPrecioR.placeholder = reparaciones5.precioContado;
+    } if (i == 6) {
+        temporalPrecioR.placeholder = reparaciones5.precioMercadoPago;
+    } if (i == 7) {
+        temporalPrecioR.placeholder = reparaciones6.precioContado;
+    } if (i == 8) {
+        temporalPrecioR.placeholder = reparaciones6.precioMercadoPago;
+    } if (i == 9) {
+        temporalPrecioR.placeholder = reparaciones7.precioContado;
+    } if (i == 10) {
+        temporalPrecioR.placeholder = reparaciones7.precioMercadoPago;
+    }
+}
